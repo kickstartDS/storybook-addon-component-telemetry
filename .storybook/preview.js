cc7c83c75b8fec1b54ddc4d039bc7f6954a5012c
Jonas Ulrich | ruhmesmeile GmbH <jonas.ulrich@ruhmesmeile.com>
2022-04-10 11:11:42 +0200

    fix: styles in canvas preview, align with design-system

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -5,6 +5,11 @@ import "@kickstartds/core/lib/design-tokens/tokens.css";
 import "@kickstartds/base/lib/global/base.js";
 import "@kickstartds/base/lib/global/base.css";
 
+import "@kickstartds/design-system/dist/index.css";
+// TODO re-add this... doesn't currently load, with `ModuleParseError: Unexpected token`
+// and `You may need an appropriate loader [..]` for `Slider.js`
+// import "@kickstartds/design-system/dist/index.js";
+
 const myActions = actions("radio");
 window.rm.radio.on("*", myActions.radio);
 
